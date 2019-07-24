ef75335609248a8d805c5199f4bb9eb278ab5d88
Paul Ewing <pcewing00@gmail.com>
2019-07-24 11:27:53 -0700

    Updates

--- run.sh
+++ run.sh
@@ -2,15 +2,23 @@
 
 kbutil_exe="src/KbUtil.Console/bin/Release/netcoreapp2.0/linux-x64/kbutil"
 input="$HOME/src/cozykeys/bloomer/bloomer.xml"
+#input="$HOME/src/cozykeys/bloomer/layouts/rev3/default/base.xml"
+#input="$HOME/src/cozykeys/bloomer/layouts/rev3/default/fn.xml"
+#input="$HOME/src/cozykeys/bloomer/layouts/rev3/default/num.xml"
 output="$HOME/src/cozykeys/svgs4"
 
 open_with="inkscape"
 #open_with="chromium-browser"
 
-#options="--visual-switch-cutouts"
-options="--visual-switch-cutouts --keycap-overlays --keycap-legends"
+options="--visual-switch-cutouts"
+#options="--visual-switch-cutouts --keycap-overlays --keycap-legends"
 
 "$kbutil_exe" gen-svg $options "$input" "$output"
 
-"$open_with" "$output/bloomer_Switch.svg"
+"$open_with" "$output/bloomer_Switch.svg" &
 
+#"$open_with" "$output/bloomer_Num.svg" &
+#"$open_with" "$output/bloomer_Fn.svg" &
+#"$open_with" "$output/bloomer_Base.svg" &
+
+wait
