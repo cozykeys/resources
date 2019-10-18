62a0f564a3fa2a004b2aa81a25026688f6cde019
Paul Ewing <pcewing00@gmail.com>
2019-10-18 16:54:02 -0700

    Update scratchpad

--- src/KbMath.Console/Commands/ScratchCommand.cs
+++ src/KbMath.Console/Commands/ScratchCommand.cs
@@ -27,14 +27,14 @@ namespace KbMath.Console.Commands
             double botLeftTheta = (180 + 45 - 10).ToRadians();
             double botRightTheta = (270 + 45 - 10).ToRadians();
 
-            //Project(-120.1, -44.983, topLeftTheta, magnitude, "01");
-            Project(-62.429, -42.938, topLeftTheta, magnitude, "02");
-            //Project(-62.429, -42.938, topLeftTheta, magnitude, "03");
-            //Project(-62.429, -42.938, topRightTheta, magnitude, "04");
-            //Project(-26.818, -25.489, topRightTheta, magnitude, "05");
-            //Project(-133.332, 30.059, botLeftTheta, magnitude, "06");
-            //Project(-40.05, 49.554, botLeftTheta, magnitude, "07");
-            //Project(-114.571, 33.367, botRightTheta, magnitude, "08");
+            Project(-120.1,  -44.983, topLeftTheta,  magnitude, "01"); // r00c00
+            Project(-62.429, -42.938, topLeftTheta,  magnitude, "02"); // r00c03
+            Project(-62.429, -42.938, topRightTheta, magnitude, "03"); // r00c03
+            Project(-26.818, -25.489, topRightTheta, magnitude, "04"); // r00c05
+            Project(-133.332, 30.059, botLeftTheta,  magnitude, "05"); // r04c00
+            Project(-114.571, 33.367, botRightTheta, magnitude, "06"); // r04c01
+            Project(-40.05,   49.554, botLeftTheta,  magnitude, "07"); // r04c05
+            Project(-21.289,  52.862, botRightTheta, magnitude, "08"); // r01c06
             
             return 0;
         }
