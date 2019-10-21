75d145877c3c91eb1d99bd281aa8670675d204be
Paul Ewing <pcewing00@gmail.com>
2019-10-21 07:44:14 -0700

    Work on Raven RGB traces

--- src/KbMath.Console/Commands/ScratchCommand.cs
+++ src/KbMath.Console/Commands/ScratchCommand.cs
@@ -1,6 +1,7 @@
 namespace KbMath.Console.Commands
 {
     using System;
+    using System.Collections.Generic;
     using KbMath.Core.Geometry2D.Extensions;
     using KbMath.Core.Geometry2D.Models;
     using Microsoft.Extensions.CommandLineUtils;
@@ -20,30 +21,61 @@ namespace KbMath.Console.Commands
 
         public int Execute()
         {
-            double magnitude = Math.Sqrt(2 * Math.Pow(18.1/2, 2));
-            
-            double topRightTheta = (45 - 10).ToRadians();
-            double topLeftTheta = (90 + 45 - 10).ToRadians();
-            double botLeftTheta = (180 + 45 - 10).ToRadians();
-            double botRightTheta = (270 + 45 - 10).ToRadians();
+            double diameter = 18.1;
+            double radius = diameter / 2;
 
-            Project(-120.1,  -44.983, topLeftTheta,  magnitude, "01"); // r00c00
-            Project(-62.429, -42.938, topLeftTheta,  magnitude, "02"); // r00c03
-            Project(-62.429, -42.938, topRightTheta, magnitude, "03"); // r00c03
-            Project(-26.818, -25.489, topRightTheta, magnitude, "04"); // r00c05
-            Project(-133.332, 30.059, botLeftTheta,  magnitude, "05"); // r04c00
-            Project(-114.571, 33.367, botRightTheta, magnitude, "06"); // r04c01
-            Project(-40.05,   49.554, botLeftTheta,  magnitude, "07"); // r04c05
-            Project(-21.289,  52.862, botRightTheta, magnitude, "08"); // r01c06
+            //double m1 = Math.Sqrt(2 * (radius * radius));
+            double m1 = 19.05 / 2.0;
+            double m2 = m1 - 0.635;
+            double m3 = m1 + 0.635;
             
+            var magnitudes = new List<double> { m1, m2, m3 };
+
+            //double topRightTheta = (45 - 10).ToRadians();
+            //double topLeftTheta = (90 + 45 - 10).ToRadians();
+            //double botLeftTheta = (180 + 45 - 10).ToRadians();
+            //double botRightTheta = (270 + 45 - 10).ToRadians();
+            //double topTheta = (90 - 10).ToRadians();
+            //double botTheta = (270 - 10).ToRadians();
+
+            double topRightTheta = (45 + 10).ToRadians();
+            double topLeftTheta = (90 + 45 + 10).ToRadians();
+            double botLeftTheta = (180 + 45 + 10).ToRadians();
+            double botRightTheta = (270 + 45 + 10).ToRadians();
+            double topTheta = (90 + 10).ToRadians();
+            double botTheta = (270 + 10).ToRadians();
+
+            
+            foreach (var m in magnitudes)
+            {
+                var vectors = new List<Vector>
+                {
+                    Project(205.126, 73.272, botTheta, m), // r01c07
+                    Project(223.018,  65.04, botTheta, m), // r01c08
+                    Project(240.737, 55.823, botTheta, m), // r01c09
+                    Project(260.366, 57.439, botTheta, m), // r01c10
+                    Project(279.647, 57.085, botTheta, m), // r01c11
+                    Project(298.408, 53.777, botTheta, m), // r01c12
+                };
+
+                for (int i = 0; i < vectors.Count - 1; ++i)
+                {
+                    Vector v0 = vectors[i];
+                    Vector v1 = vectors[i+1];
+
+                    string start = $"(start {v0.X} {v0.Y})";
+                    string end = $"(end {v1.X} {v1.Y})";
+                    Console.WriteLine($"(segment {start} {end} (width 0.2032) (layer Back) (net 5))");
+                }
+            }
+
             return 0;
         }
 
-        private void Project(double x, double y, double theta, double magnitude, string name)
-        {
-            var projection = new Vector(x, -y).Project(theta, magnitude);
-            Console.WriteLine($"<Constant Name=\"X{name}\" Value=\"{projection.X}\" />");
-            Console.WriteLine($"<Constant Name=\"Y{name}\" Value=\"{-projection.Y}\" />");
+        private Vector Project(double x, double y, double theta, double magnitude)
+        { 
+            var v = new Vector(x, -y).Project(theta, magnitude);
+            return new Vector(v.X, -v.Y);
         }
     }
 }
